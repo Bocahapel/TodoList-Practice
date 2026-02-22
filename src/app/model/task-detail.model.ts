aa24baf30424f71087f4b9a4d5930eb1a4f18d97
Bocahapel <pratamadrian.putra@gmail.com>
2026-02-22 15:47:32 +0700

    Update task component
    -adjust default font
    -adjust default sidebar color

---
+++ src/app/model/task-detail.model.ts
@@ -0,0 +1,9 @@
+export interface DataTaskDetail {
+  taskId: number;
+  taskTitle: string;
+  taskSubTitle: string;
+  taskDescription: string;
+  startDate: string;
+  dueDate: string;
+  additionalNote: string | null;
+}
